517aeb4397a7a41412953e400f31d7c6e2d5f18d
Donghyeon <dlehdgus1207@naver.com>
2025-12-21 17:36:43 +0900

    카카오 + JWT 추가

--- src/main/java/capstone/fridge/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/capstone/fridge/domain/member/domain/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package capstone.fridge.domain.member.domain.repository;
 import capstone.fridge.domain.member.domain.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByKakaoId(String kakaoId);
 }
--- src/main/java/capstone/fridge/global/config/SecurityConfig.java
+++ src/main/java/capstone/fridge/global/config/SecurityConfig.java
@@ -1,34 +1,34 @@
-
 package capstone.fridge.global.config;
 
+import capstone.fridge.global.config.security.JwtAuthenticationFilter;
+import capstone.fridge.global.config.security.JwtTokenProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@Profile("dev")
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtTokenProvider jwtTokenProvider) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/fridge/**",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
-                        ).permitAll()
-                        .anyRequest().permitAll() // dev는 일단 다 열어두자
-                );
+                        .requestMatchers("/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/fridge/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
 }
+
 
 //package capstone.fridge.global.config;
 //
